394e6727d8d860180dcbe48313f5261e56cb1e9f
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2023-01-29 16:41:55 -0300

    implementing currency converter

--- oo/strings/tests_url_extractor.py
+++ oo/strings/tests_url_extractor.py
@@ -1,9 +1,20 @@
+from decimal import Decimal
+from forex_python.converter import CurrencyRates
+
 from url_extractor import URLExtractor
 
+UNITED_STATES_CODE = 'USD'
+BRAZILIAN_CODE = 'BRL'
 
-url = URLExtractor('https://bytebank.com/cambio?q=python&sId=1233423123')
 
-print(url.get_base_url())
-print(url.get_params_url())
-print(url.get_param_value('q'))
-print(url.get_param_value('sId'))
+url = URLExtractor('bytebank.com/cambio?quantidade=100&moedaOrigem=dolar&moedaDestino=real')
+
+source_currency = url.get_param_value('moedaOrigem')
+source_currency_code = UNITED_STATES_CODE if source_currency == 'dolar' else BRAZILIAN_CODE 
+destination_currency = url.get_param_value('moedaDestino')
+destination_currency_code = UNITED_STATES_CODE if destination_currency == 'dolar' else BRAZILIAN_CODE 
+amount = Decimal(url.get_param_value('quantidade'))
+
+currency_rates = CurrencyRates()
+converted_value = currency_rates.convert(source_currency_code, destination_currency_code, amount)
+print(converted_value)
